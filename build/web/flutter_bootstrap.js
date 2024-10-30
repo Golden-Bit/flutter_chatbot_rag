70f209f44e3de3e8df4ad1f8bc7a7638404ae73e
sans <simonesansalone777@gmail.com>
2024-10-30 09:57:00 +0100

    update services addresses in code

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3717370610"
+    serviceWorkerVersion: "982446706"
   }
 });

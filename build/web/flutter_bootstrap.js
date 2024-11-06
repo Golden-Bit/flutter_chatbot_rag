e5b6219a23de7ad2aa814be20842366ad67be5c5
sans <simonesansalone777@gmail.com>
2024-11-06 04:12:43 +0100

    sync remote with local changes

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2792273286"
+    serviceWorkerVersion: "4003372121"
   }
 });

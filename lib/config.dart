a311ded3627bdaaba1239262c98204a679915e9d
sans <simonesansalone777@gmail.com>
2024-11-25 01:34:14 +0100

    sync remote with local changes

--- lib/config.dart
+++ lib/config.dart
@@ -3,6 +3,14 @@ config = {
     "backend_api": "http://34.140.110.56:8095",
     "nlp_api": "http://34.140.110.56:8100",
     "chatbot_nlp_api": "http://34.140.110.56:8080"
-}*/
+}
+
+config = {
+    "backend_api": "https://teatek-llm.theia-innovation.com/user-backend",
+    "nlp_api": "https://teatek-llm.theia-innovation.com/llm-core",
+    "chatbot_nlp_api": "https://teatek-llm.theia-innovation.com/llm-rag"
+}
+
+*/
 
 //34.79.136.231

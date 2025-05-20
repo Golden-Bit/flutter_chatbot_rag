ffa5c6c53c328e7df0a928b6317ae82a141d9159
GOLD SOLAR s.r.l <info@goldsolarweb.com>
2025-05-20 13:05:35 +0200

    sync remote with local changes

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -32,14 +32,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "fc12317e24c86587a1ab513de5646197
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "7367bba186dec14aaf36fe41e24ec8e8",
+"flutter_bootstrap.js": "c73a580767210fcc604f9a07a03ab494",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "ca91222a3354b7f5bb6344bcd80f7ec3",
 "/": "ca91222a3354b7f5bb6344bcd80f7ec3",
-"main.dart.js": "eaf323d38e428dfddeeaeb318b30dac7",
+"main.dart.js": "127bf491213cb181cbac97e060d659a5",
 "manifest.json": "71e0154c18320c9ca4cbf26c3e30935d",
 "version.json": "322dfa94430cc69aa45ce83495f46466"};
 // The application shell files that are downloaded before a service worker can

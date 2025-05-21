a4bf4dc72dde4e59bcdf954e5711f1c66ba92b4c
GOLD SOLAR s.r.l <info@goldsolarweb.com>
2025-05-21 16:05:48 +0200

    sync remote with local changes

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3780345350"
+    serviceWorkerVersion: "3487551571"
   }
 });

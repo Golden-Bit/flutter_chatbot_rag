f9deac05c860bc3e0c43a8f539fb8c779fac34b8
GOLD SOLAR s.r.l <info@goldsolarweb.com>
2025-06-09 14:52:03 +0200

    sync remote with local changes

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -32,7 +32,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "fc12317e24c86587a1ab513de5646197
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "13b630c244b6f68d653a9e8762a6aae9",
+"flutter_bootstrap.js": "572600b03732c3b750f1ee5c00ed90d2",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
